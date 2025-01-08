52545da6686889827d20c751004d3129c886dc0d
Hexenone <101699955+Hexenone@users.noreply.github.com>
2025-01-08 15:39:32 +0700

    Todo

--- react-progress/src/App.jsx
+++ react-progress/src/App.jsx
@@ -1,19 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Todo from "./components/Todo.jsx";
+import Form from "./components/Form.jsx";
+import FilterButton from "./components/FilterButton.jsx"
 
 function App(props) {
-  const [count, setCount] = useState(0)
-
+  const taskList = props.tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
   return (
-    <>
-      <div>
-        <h1>{props.greeting} {props.subject}!</h1>
-        <button type="button" classNane="primary">Click me!</button>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-    </>
-  )
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
